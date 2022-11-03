21d193f65e19e3e72ca6bbd4a90ee01e78d0a3d7
elinarou <elina_rouvinen@outlook.com>
2022-11-03 21:43:53 +0200

    Lisatty Clear-ominaisuuden testaus

--- src/App.js
+++ src/App.js
@@ -10,17 +10,22 @@ function App() {
     event.preventDefault();
     setTodos([...todos, todo]);
     setTodo({desc: '', date: ''});
-  }
+  };
+
+  const clearTodos = () => {
+    setTodos([]);
+  };
 
   const inputChanged = (event) => {
     setTodo({...todo, [event.target.name]: event.target.value});
-  } 
+  }; 
 
   return (
     <div className="App">
       <input type="text" placeholder="Date" name="date" value={todo.date} onChange={inputChanged}/>
       <input type="text" placeholder="Description" name="desc" value={todo.desc} onChange={inputChanged}/>
       <button onClick={addTodo}>Add</button>
+      <button onClick={clearTodos}>Clear</button>
       <TodoTable todos={todos} />
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -13,3 +13,16 @@ test('add todo', () => {
   const tablecell = screen.getByText(/harjoittele kokeeseen/i);
   expect(tablecell).toBeInTheDocument();
 });
+
+test('clear todos', () => {
+  render(<App />);
+  const desc = screen.getByPlaceholderText('Description');
+  fireEvent.change(desc, { target: { value: 'Harjoittele kokeeseen'}});
+  const date = screen.getByPlaceholderText('Date');
+  fireEvent.change(date, { target: { value: '3.11.2022'}});
+  const button = screen.getByText('Clear');
+  fireEvent.click(button);
+  const tablecell = screen.queryByText(/harjoittele kokeeseen/i);
+  expect(tablecell).not.toBeInTheDocument();
+});
+
